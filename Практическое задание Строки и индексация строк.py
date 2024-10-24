90e1962dec8269bc77f007da544d2a6b1fa6f53c
TimurHaliullin <89094568+TimurHaliullin@users.noreply.github.com>
2024-10-24 18:38:23 +0300

    Add files via upload

--- Практическое задание Строки и индексация строк.py
+++ Практическое задание Строки и индексация строк.py
@@ -1,3 +1,7 @@
-name = 'Habib Nurmagomedov'
-print(name[6])
-print(name[:7])
+my_string = 'это моя строка'
+print(my_string[0])
+print(my_string[-1])
+print(my_string[2:5])
+print(my_string[::-1])
+print(len(my_string))
+print("это моя новая строка" + '' + my_string)
