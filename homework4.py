b4ef9e44abf243860a1db16bc4757839bf48c905
TimurHaliullin <89094568+TimurHaliullin@users.noreply.github.com>
2024-10-25 16:52:21 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,5 @@
+immutable_var = tuple = (1, "Hello", True, 3.14)
+print("immutable_var:", immutable_var)
+mutable_list = [1, "World", False, 2.71]
+mutable_list[1] = "Python"
+print("mutable_list:", mutable_list)
