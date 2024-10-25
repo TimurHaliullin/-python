959cedd883a80c95814e82329021f1bf8919852a
TimurHaliullin <89094568+TimurHaliullin@users.noreply.github.com>
2024-10-25 18:27:15 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,12 @@
+my_list = ['Apple', 'Milk', 'Cheese', "Kalbasa"]
+print("List:",my_list)
+print("First element:",my_list[0])
+print("Last element:",my_list[-1])
+print("Sublist:",my_list[2:4])
+my_list[2] = "Сыр"
+print("Modified list:", my_list)
+my_dict = {"Apple":"Яблока","Milk":"Молоко","Cheese":"Сыр","Kalbasa":"Калбаса"}
+print("Dictionary:", my_dict)
+print("Translation:", my_dict["Cheese"])
+my_dict["Milk"] = "Молоко"
+print("Modified dictionary:", my_dict )
